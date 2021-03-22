c85042de8185c42feefd193c2933b273731ac002
Gabor Recski <gabor.recski@tuwien.ac.at>
2021-03-22 11:19:41 +0100

    vocab filtering updates

--- filter_vocab.py
+++ filter_vocab.py
@@ -1,15 +1,63 @@
-import sys
+"""filter words from a vocabulary and from a BOW matrix"""
+import argparse
+
+import numpy as np
+from scipy.sparse import load_npz, save_npz
+from tuw_nlp.common.vocabulary import Vocabulary
+
+from common import STOPWORDS as SW
 
 
-def keep(word):
-    return word.isalpha()
+def filter_vocab(vocab, A):
+    print(f'filtering matrix of shape {A.shape}...')
+    word_freqs = np.squeeze(np.asarray(A.sum(axis=0)))
+    rel_doc_counts = np.squeeze(np.asarray(
+        A.astype(bool).sum(axis=0) / A.shape[0]))
+
+    new_vocab = Vocabulary()
+    id_map = {}
+    for i, word in vocab.id_to_word.items():
+        # if not word.istitle():
+        #    continue
+        if word_freqs[i] >= 100000:
+            continue
+        if len(word) <= 2:
+            continue
+        if rel_doc_counts[i] >= 0.9:
+            continue
+        if word.lower() in SW:
+            continue
+
+        new_vocab.add(word)
+        id_map[i] = new_vocab.get_id(word)
+
+    B = A[:, tuple(id_map.keys())]
+    print('new matrix shape:', B.shape)
+    return new_vocab, B
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-c", "--corpus-file", default=None, type=str)
+    parser.add_argument("-d", "--new-corpus", default=None, type=str)
+    parser.add_argument("-v", "--vocab-file", default=None, type=str)
+    parser.add_argument("-w", "--new-vocab", default=None, type=str)
+    return parser.parse_args()
 
 
 def main():
-    for line in sys.stdin:
-        word = line.strip().split('\t')[0]
-        if keep(word):
-            sys.stdout.write(line)
+    args = get_args()
+
+    print('loading...')
+    vocab = Vocabulary.from_file(args.vocab_file)
+    bow_matrix = load_npz(args.corpus_file)
+
+    print('filtering...')
+    new_vocab, new_matrix = filter_vocab(vocab, bow_matrix)
+
+    print('saving...')
+    new_vocab.to_file(args.new_vocab)
+    save_npz(args.new_corpus, new_matrix)
 
 
 if __name__ == "__main__":
--- lda_train.py
+++ lda_train.py
@@ -5,33 +5,38 @@ import joblib
 # from gensim.models import LdaMulticore
 from scipy.sparse import load_npz
 from sklearn.decomposition import LatentDirichletAllocation
-# from tuw_nlp.common.vocabulary import Vocabulary
+from tuw_nlp.common.vocabulary import Vocabulary
+
+from filter_vocab import filter_vocab
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-m", "--model-file", type=str)
     parser.add_argument("-c", "--corpus-file", default=None, type=str)
-    # parser.add_argument("-v", "--vocab-file", default=None, type=str)
     parser.add_argument("-n", "--n-topics", default=None, type=int)
-    parser.add_argument("-w", "--n-workers", default=None, type=int)
+    parser.add_argument("-j", "--n-workers", default=None, type=int)
+    parser.add_argument("-v", "--vocab-file", default=None, type=str)
+    parser.add_argument("-w", "--new-vocab", default=None, type=str)
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    # vocab = Vocabulary.from_file(args.vocab_file)
+    print('loading vocabulary...')
+    vocab = Vocabulary.from_file(args.vocab_file)
     print('loading corpus...')
     bow_matrix = load_npz(args.corpus_file)
-    # corpus = Sparse2Corpus(bow_matrix)
-    print('training LDA model...')
-    # lda = LdaMulticore(
-    #    corpus, id2word=vocab.id_to_word, num_topics=args.n_topics,
-    #    workers=args.n_workers)
 
+    print('filtering...')
+    new_vocab, new_matrix = filter_vocab(vocab, bow_matrix)
+    print('saving filtered vocabulary...')
+    new_vocab.to_file(args.new_vocab)
+
+    print('training LDA model...')
     lda = LatentDirichletAllocation(
         n_components=args.n_topics, n_jobs=args.n_workers, random_state=0)
-    lda.fit(bow_matrix)
+    lda.fit(new_matrix)
 
     print('saving model...')
     joblib.dump(lda, args.model_file)
